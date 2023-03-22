ea2469391e02142d0bb90f53930a68bcef1237c6
tomarayush8624 <ayushtomar8624@gamil.com>
2023-03-23 02:18:42 +0530

    without refactoring

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const players0El = document.querySelector('#name--0');
-const player1El = document.querySelector('#name--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+// const players0El = document.querySelector('#name--0');
+// const player1El = document.querySelector('#name--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.querySelector('#current--0');
@@ -26,37 +28,65 @@ function selectPlayer(player) {
 }
 
 // Initial Consitions
-diceEl.classList.add('hidden');
-score0El.textContent = 0;
-score1El.textContent = 0;
 let player = 0;
+let gameContinue = true;
+
+function reset() {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player = 0;
+}
+reset();
 let currentNum = current0El;
 let totalScore = score0El;
 
 // console.log(typeof current0El);
 
 rollBtn.addEventListener('click', function () {
-  let num = Math.ceil(Math.random() * 6);
-  // num = 1;
-  // console.log(num);
-  selectPlayer(player);
-  changeImage(num);
-  if (num == 1) {
-    currentNum.textContent = 0;
-
-    player = player === 1 ? 0 : 1;
-  } else {
-    currentNum.textContent = Number(currentNum.textContent) + num;
+  if (gameContinue) {
+    let num = Math.ceil(Math.random() * 6);
+    selectPlayer(player);
+    changeImage(num);
+    if (num == 1) {
+      currentNum.textContent = 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+      player = player === 1 ? 0 : 1;
+    } else {
+      currentNum.textContent = Number(currentNum.textContent) + num;
+    }
   }
 });
 
 holdBtn.addEventListener('click', function () {
-  totalScore.textContent =
-    Number(totalScore.textContent) + Number(currentNum.textContent);
-  currentNum.textContent = 0;
-  if (Number(totalScore.textContent) >= 100) {
-    console.log('You won');
-  } else {
-    player = player === 1 ? 0 : 1;
+  if (gameContinue) {
+    totalScore.textContent =
+      Number(totalScore.textContent) + Number(currentNum.textContent);
+    currentNum.textContent = 0;
+    if (Number(totalScore.textContent) >= 30) {
+      console.log('You won');
+      gameContinue = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${player}`)
+        .classList.add('player--winner');
+    } else {
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
+      player = player === 1 ? 0 : 1;
+    }
   }
+});
+
+newBtn.addEventListener('click', function () {
+  diceEl.classList.remove('hidden');
+  document
+    .querySelector(`.player--${player}`)
+    .classList.remove('player--winner');
+  reset();
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  gameContinue = true;
 });
